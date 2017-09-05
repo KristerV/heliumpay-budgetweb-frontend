3aab573aa8ef2a495df35f305e058078c862527a
Krister Viirsaar <krister.viirsaar@gmail.com>
2017-09-05 10:51:51 +0300

    Submit proposal

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,33 @@ import 'isomorphic-fetch'
 import 'moment'
 
 export default class extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
 	static async getInitialProps () {
+		let errors = []
+
 		const proposalsResult = await fetch(config.apiUrl+'/v0/core/proposals?status=active')
-		const proposals = await proposalsResult.json()
+		let proposals = await proposalsResult.json()
+		if (proposalsResult.status !== 200) {
+			errors.push(proposals.message)
+			proposals = []
+		}
 
 		const budgetResult = await fetch(config.apiUrl+'/v0/core/budget')
-		const budget = await budgetResult.json()
+		let budget = await budgetResult.json()
+		if (budgetResult.status !== 200) {
+			errors.push(budget.message)
+			budget = {}
+		}
 
 		return {
 			proposals,
-			budget
+			budget,
+			errors
 		}
 	}
 
@@ -25,12 +42,19 @@ export default class extends React.Component {
 		const header = <div>
 				<p>There are {this.props.proposals.length} active proposals (not counting closed).</p>
 				<p>Next payment will be {Math.round(budget.budgetTotal * 100) / 100} {config.ticker} and it will occur in {Math.round(budget.paymentDelay/60/60/24)} days (however there are only {Math.round(budget.voteDeadlineDelay/60/60/24)} days to vote).</p>
+				<p><Link href="/proposal/submit"><a>Create proposal</a></Link></p>
 			</div>
 		return (
 			<Layout header={header}>
+				<p className="error">{this.props.errors}</p>
 				{this.props.proposals.map((p, i) => {
 					return <Item key={i} data={p}/>
 				})}
+				<style jsx>{`
+					.error {
+						color: red;
+					}
+				`}</style>
 			</Layout>
 		)
 	}
